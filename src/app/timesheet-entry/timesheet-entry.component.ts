4cf2a8e6af2d1807c561c8178749b8390803326f
Kaiser Sayeed <kaiser.sayeed07@gmail.com>
2019-10-07 20:47:44 +1100

    added dynamic col structure

--- src/app/timesheet-entry/timesheet-entry.component.ts
+++ src/app/timesheet-entry/timesheet-entry.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TimeSheetService} from './services/timesheet.service';
 import {Task} from './models/timesheet.model';
+import {SelectItem} from "primeng/components/common/selectitem";
 
 @Component({
   selector: 'app-timesheet-entry',
@@ -9,7 +10,8 @@ import {Task} from './models/timesheet.model';
 })
 export class TimesheetEntryComponent implements OnInit {
   tasks: Task[];
-  taskTypes: any;
+  taskTypes: SelectItem[];
+  cols: any;
 
   constructor(private timeSheetService: TimeSheetService) {
   }
@@ -20,6 +22,23 @@ export class TimesheetEntryComponent implements OnInit {
     });
 
     this.taskTypes = this.timeSheetService.getTaskTypes();
+
+    this.cols = [
+      {field: 'taskEditState', header: 'State', type: 'readonly'},
+      {field: 'title', header: 'Title', type: 'text', getErrorMessage: this.validateTitleField},
+      {field: 'type', header: 'Type', type: 'dropdown', optionItems: this.taskTypes},
+      {field: 'duration', header: 'Duration', type: 'text', getErrorMessage: this.validateDuration},
+      {field: 'hourlyRate', header: 'Hourly Rate', type: 'number', nonEditable: '$'},
+      {field: 'total', header: 'Total', type: 'readonly', nonEditable: '$'},
+    ];
+  }
+
+  validateDuration(duration: string): string | null {
+    return '';
+  }
+
+  validateTitleField(title: string): string | null {
+    return '';
   }
 }
 
--- src/app/timesheet-entry/timesheet-entry.module.ts
+++ src/app/timesheet-entry/timesheet-entry.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { TimesheetEntryRoutingModule } from './timesheet-entry-routing.module';
-import { TimesheetEntryComponent } from './timesheet-entry.component';
+import {TimesheetEntryRoutingModule} from './timesheet-entry-routing.module';
+import {TimesheetEntryComponent} from './timesheet-entry.component';
 
+import {TableModule} from 'primeng/table';
+import {DropdownModule} from 'primeng/dropdown';
+import {ButtonModule} from 'primeng/button';
+import {DialogModule} from "primeng/dialog";
 
 @NgModule({
   declarations: [
@@ -11,7 +17,14 @@ import { TimesheetEntryComponent } from './timesheet-entry.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    HttpClientModule,
     TimesheetEntryRoutingModule,
+    TableModule,
+    DropdownModule,
+    ButtonModule,
+    DialogModule,
   ]
 })
-export class TimesheetEntryModule { }
+export class TimesheetEntryModule {
+}
